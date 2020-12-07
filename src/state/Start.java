46547738028e0038e26498f38d2caf0f464d79b5
stepJo <ephenjo99@gmail.com>
2020-12-07 15:19:10 +0700

    Fix formatting space and curly brace

--- src/state/Start.java
+++ src/state/Start.java
@@ -1,11 +1,12 @@
 package state;
 
-public class Start extends TimerState{
+public class Start extends TimerState {
 	TimerState Source;
 	int second;
 	int minute;
 	int elapsedTime;
-	public Start(TimerState Source , int second , int minute, int elapsedTime) {
+	
+	public Start(TimerState Source, int second, int minute, int elapsedTime) {
 		this.Source = Source;
 		this.second = second;
 		this.minute = minute;
@@ -22,11 +23,12 @@ public class Start extends TimerState{
 	@Override
 	public TimerState increment() {
 		if(second >= 60) {
-			elapsedTime = elapsedTime+1000;
-			return new Start(this, (elapsedTime/1000) % 60  , (elapsedTime/60000) % 60 , elapsedTime);
-		} else {
-			elapsedTime = elapsedTime+1000;
-			return new Start(this, (elapsedTime/1000) % 60  , (elapsedTime/60000) % 60 , elapsedTime);
+			elapsedTime = elapsedTime + 1000;
+			return new Start(this, (elapsedTime / 1000) % 60, (elapsedTime / 60000) % 60, elapsedTime);
+		} 
+		else {
+			elapsedTime = elapsedTime + 1000;
+			return new Start(this, (elapsedTime / 1000) % 60, (elapsedTime / 60000) % 60, elapsedTime);
 		}
 	}
 
--- src/state/Timers.java
+++ src/state/Timers.java
@@ -4,7 +4,7 @@ public class Timers {
 	private TimerState currentState;
 	
 	public Timers(int elapsedTime) {
-		currentState = new Start(currentState , (elapsedTime/1000) % 60 , (elapsedTime/60000) % 60, elapsedTime);
+		currentState = new Start(currentState, (elapsedTime / 1000) % 60 , (elapsedTime / 60000) % 60, elapsedTime);
 	}
 	
 	public void Start() {
