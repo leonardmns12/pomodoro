a31c4e277b6ef26ed526bc2c8623b56b48d2e319
michaelfirdaus <michaelfirdausmf@gmail.com>
2020-12-07 22:30:07 +0700

    Fixing Pomodoro Short Break Time

--- src/state/TimerState.java
+++ src/state/TimerState.java
@@ -1,11 +1,13 @@
 package state;
 
 public abstract class TimerState {
-	protected abstract String getTime();
 	
-	public abstract TimerState increment();
-	public abstract TimerState shortBreak();
-	public abstract TimerState longBreak();
+	protected abstract String getTime();
+	public abstract int currentTime();
+	protected abstract void resetTime();
+	protected abstract TimerState increment();
+	protected abstract TimerState pomodoro();
+	protected abstract TimerState shortBreak();
 	
 	public String print() {
 		return getTime();
